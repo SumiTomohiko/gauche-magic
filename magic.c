87a8e180ddbdf8c79fbd1b2491e7f0244d6d892e
tom <tom@896846e3-eb4a-0410-97fc-6743a5b48e86>
2008-04-15 21:09:10 +0000

    * magic.h: Added Scm_Magic.
    git-svn-id: file:///home/tom/svn/magic/trunk@3 896846e3-eb4a-0410-97fc-6743a5b48e86

--- magic.c
+++ magic.c
@@ -4,10 +4,22 @@
 
 #include "magic.h"
 
-/*
- * The following function is a dummy one; replace it for
- * your C function definitions.
- */
+static void 
+magic_print(ScmObj obj, ScmPort *port, ScmWriteContext *ctx) 
+{
+    ScmMagic* magic = SCM_MAGIC(obj);
+    Scm_Printf(port, "#<magic>");
+}
+
+SCM_DEFINE_BUILTIN_CLASS_SIMPLE(Scm_MagicClass, magic_print);
+
+ScmObj Scm_OpenMagic()
+{
+    ScmMagic* magic = SCM_NEW(ScmMagic);
+    SCM_SET_CLASS(magic, SCM_CLASS_MAGIC);
+    magic->magic = magic_open(MAGIC_NONE);
+    return SCM_OBJ(magic);
+}
 
 ScmObj test_magic(void)
 {
@@ -31,4 +43,9 @@ void Scm_Init_magic(void)
 
     /* Register stub-generated procedures */
     Scm_Init_magiclib(mod);
+
 }
+
+/**
+ * vim: tabstop=4 shiftwidth=4 expandtab softtabstop=4
+ */
--- magic.h
+++ magic.h
@@ -8,17 +8,27 @@
 
 #include <gauche.h>
 #include <gauche/extend.h>
+#include <magic.h>
 
 SCM_DECL_BEGIN
 
-/*
- * The following entry is a dummy one.
- * Replace it for your declarations.
- */
+typedef struct ScmMagicRec {
+    SCM_HEADER;
+    magic_t magic;
+} ScmMagic;
+
+SCM_CLASS_DECL(Scm_MagicClass);
+#define SCM_CLASS_MAGIC (&Scm_MagicClass)
+#define SCM_MAGIC(obj)  ((ScmMagic*)obj)
 
 extern ScmObj test_magic(void);
+extern ScmObj Scm_OpenMagic();
 
 /* Epilogue */
 SCM_DECL_END
 
 #endif  /* GAUCHE_MAGIC_H */
+
+/**
+ * vim: tabstop=4 shiftwidth=4 expandtab softtabstop=4 
+ */
